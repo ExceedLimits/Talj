fb2554da055f7dd3ac5e3321137919b3b456bdf2
ExceedLimits <ammar.exceed@gmail.com>
2024-02-24 20:56:13 +0200

    Config.php update

--- config.php
+++ config.php
@@ -1,6 +1,4 @@
 <?php
-
-
 require_once("includes\autoloader.php");
 require_once("includes\Router.php");
 
@@ -8,7 +6,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 define('BASE_PATH', (dirname(__FILE__)));
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
-
 
 const APP_NAME = 'Dashboard';
 const APP_URL= 'http://localhost/framework';
