57ce8ac9e4fb229104216d3bab615098bf173861
ExceedLimits <ammar.exceed@gmail.com>
2024-02-25 11:48:33 +0200

    Config.php update

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ define('BASE_PATH', (dirname(__FILE__)));
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 
 const APP_NAME = 'Dashboard';
-const APP_URL= 'http://localhost/framework';
+const APP_URL= 'http://localhost/Talj';
 const ASSET_URL= APP_URL.'/assets/';
 
